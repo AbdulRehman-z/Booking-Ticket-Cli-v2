8761722fbd7b1a9b604d0a34c2e06bd03308d66c
Abdul Rehman <yousafbhaikhan10@gmail.com>
2022-12-05 11:03:59 +0500

    added concurrency

--- pakistan/pakistanConf.go
+++ pakistan/pakistanConf.go
@@ -3,7 +3,11 @@ package pakistan
 import (
 	"Golang/helper"
 	"fmt"
+	"sync"
+	"time"
 )
+
+var wg = sync.WaitGroup{}
 
 func PakistanConference() {
 	const conferenceTickets uint = 40
@@ -13,30 +17,41 @@ func PakistanConference() {
 
 	// Greet users to the conference
 	helper.GreetUsers(conferenceTickets, remainingTickets, conferenceName)
-	for {
-		// Get user Inputs
-		userData := helper.GetUserInputs()
-		// Validate User Inputs
-		isValidUsername, isValidEmail, isValidBooking := helper.ValidateUserInputs(userData.FirstName, userData.LastName, userData.Email, userData.BookedTickets, remainingTickets)
 
-		if isValidUsername && isValidEmail && isValidBooking {
+	// Get user Inputs
+	userData := helper.GetUserInputs()
+	// Validate User Inputs
+	isValidUsername, isValidEmail, isValidBooking := helper.ValidateUserInputs(userData.FirstName, userData.LastName, userData.Email, userData.BookedTickets, remainingTickets)
 
-			bookings = append(bookings, userData.FirstName)
-			remainingTickets = remainingTickets - userData.BookedTickets
+	if isValidUsername && isValidEmail && isValidBooking {
 
-			firstNames := helper.GetFirstNames(bookings)
-			fmt.Printf("Congratulations %v %v 🎉. You have booked %v tickets. A conformation email will be sent at %v.\n",
-				userData.FirstName, userData.LastName, userData.BookedTickets, userData.Email)
+		bookings = append(bookings, userData.FirstName)
+		remainingTickets = remainingTickets - userData.BookedTickets
+		wg.Add(1)
+		go sendTicket(userData.FirstName)
 
-			// Details for administration about the bookings
-			helper.AdministrationDetails(firstNames, bookings, userData.BookedTickets, remainingTickets)
-			// Check if remainingTickets == 0
-			if remainingTickets == 0 {
-				helper.ZeroRemainingTickets(firstNames)
-				break
-			}
-		} else {
-			helper.ErrorReport(isValidUsername, isValidEmail, isValidBooking, userData.FirstName, userData.Email, remainingTickets)
+		firstNames := helper.GetFirstNames(bookings)
+		fmt.Printf("Congratulations %v %v 🎉. You have booked %v tickets. A conformation email will be sent at %v.\n",
+			userData.FirstName, userData.LastName, userData.BookedTickets, userData.Email)
+
+		// Details for administration about the bookings
+		helper.AdministrationDetails(firstNames, bookings, userData.BookedTickets, remainingTickets)
+		// Check if remainingTickets == 0
+		if remainingTickets == 0 {
+			helper.ZeroRemainingTickets(firstNames)
 		}
+	} else {
+		helper.ErrorReport(isValidUsername, isValidEmail, isValidBooking, userData.FirstName, userData.Email, remainingTickets)
 	}
+
+	wg.Wait()
+}
+
+func sendTicket(firstName string) {
+	time.Sleep(20 * time.Second)
+	ticket := fmt.Sprintf("%v booked ticket!", firstName)
+	fmt.Println("##################")
+	fmt.Printf("%v\n", ticket)
+	fmt.Println("##################")
+	wg.Done()
 }
